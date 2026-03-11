8441a6da4e73a38a526392c3317e40c03629e177
Rafaa Omar <rafaa.ommar@gmail.com>
2026-03-12 01:56:11 +0300

    external project modification

---
+++ ExternalAuth/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace OAuthClient.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet("/callback")]
+        public async Task<IActionResult> Callback(string code, string state)
+        {
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+            var client = new HttpClient(handler);
+            var response = await client.PostAsync("https://localhost:7112/oauth/token", new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("code", code),
+                new KeyValuePair<string, string>("client_id", "client_app")
+            }));
+
+            var content = await response.Content.ReadAsStringAsync();
+            var tokenResponse = JsonSerializer.Deserialize<Dictionary<string, object>>(content);
+
+            return Content($"Access Token: {tokenResponse["access_token"]}");
+        }
+    }
+}
